fa52aa4e1e3cc358a5916b338c55aa01f4d23b3b
Bikash <bikash11roy@gmail.com>
2018-04-08 16:45:19 +0600

    train the model

--- models/attention_model/seq2seq_attention.py
+++ models/attention_model/seq2seq_attention.py
@@ -12,6 +12,8 @@ import datetime
 import math
 import socket
 
+hostname = socket.gethostname()
+
 import torch.nn as nn
 from torch.autograd import Variable
 from torch import optim
@@ -19,6 +21,22 @@ import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 from utils.Lang import Lang
 from models.attention_model.encoder_decoder import *
+import io
+import torchvision
+from PIL import Image
+import visdom
+import matplotlib.ticker as ticker
+
+vis = visdom.Visdom()
+import numpy as np
+import matplotlib.pyplot as plt
+
+PAD_token = 0
+SOS_token = 1
+EOS_token = 2
+
+MIN_LENGTH = 3
+MAX_LENGTH = 25
 
 
 # Helper functions
@@ -180,6 +198,225 @@ def random_batch(batch_size):
     return input_var, input_lengths, target_var, target_lengths
 
 
+def test_model():
+    # Test the model
+    print("\n---------------------")
+    print("Testing the model....")
+    small_batch_size = 3
+    input_batches, input_lengths, target_batches, target_lengths = random_batch(small_batch_size)
+
+    print('input_batches', input_batches.size())  # (max_len x batch_size)
+    print('target_batches', target_batches.size())  # (max_len x batch_size)
+
+    small_hidden_size = 8
+    small_n_layers = 2
+
+    encoder_test = EncoderRNN(input_lang.n_words, small_hidden_size, small_n_layers)
+    decoder_test = LuongAttnDecoderRNN('general', small_hidden_size, output_lang.n_words, small_n_layers)
+
+    if USE_CUDA:
+        encoder_test.cuda()
+        decoder_test.cuda()
+
+    encoder_outputs, encoder_hidden = encoder_test(input_batches, input_lengths, None)
+
+    print('encoder_outputs', encoder_outputs.size())  # max_len x batch_size x hidden_size
+    print('encoder_hidden', encoder_hidden.size())  # n_layers * 2 x batch_size x hidden_size
+
+    max_target_length = max(target_lengths)
+
+    # Prepare decoder input and outputs
+    decoder_input = Variable(torch.LongTensor([SOS_token] * small_batch_size))
+    decoder_hidden = encoder_hidden[:decoder_test.n_layers]  # Use last (forward) hidden state from encoder
+    all_decoder_outputs = Variable(torch.zeros(max_target_length, small_batch_size, decoder_test.output_size))
+
+    if USE_CUDA:
+        all_decoder_outputs = all_decoder_outputs.cuda()
+        decoder_input = decoder_input.cuda()
+
+    # Run through decoder one time step at a time
+    for t in range(max_target_length):
+        decoder_output, decoder_hidden, decoder_attn = decoder_test(
+            decoder_input, decoder_hidden, encoder_outputs
+        )
+        all_decoder_outputs[t] = decoder_output  # Store this step's outputs
+        decoder_input = target_batches[t]  # Next input is current target
+
+    # Test masked cross entropy loss
+    loss = masked_cross_entropy(
+        all_decoder_outputs.transpose(0, 1).contiguous(),
+        target_batches.transpose(0, 1).contiguous(),
+        target_lengths
+    )
+    print('loss', loss.data[0])
+
+
+def train(input_batches, input_lengths, target_batches, target_lengths, encoder, decoder, encoder_optimizer,
+          decoder_optimizer, criterion, max_length=MAX_LENGTH):
+    # Zero gradients of both optimizers
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+    loss = 0  # Added onto for each word
+
+    # Run words through encoder
+    encoder_outputs, encoder_hidden = encoder(input_batches, input_lengths, None)
+
+    # Prepare input and output variables
+    decoder_input = Variable(torch.LongTensor([SOS_token] * batch_size))
+    decoder_hidden = encoder_hidden[:decoder.n_layers]  # Use last (forward) hidden state from encoder
+
+    max_target_length = max(target_lengths)
+    all_decoder_outputs = Variable(torch.zeros(max_target_length, batch_size, decoder.output_size))
+
+    # Move new Variables to CUDA
+    if USE_CUDA:
+        decoder_input = decoder_input.cuda()
+        all_decoder_outputs = all_decoder_outputs.cuda()
+
+    # Run through decoder one time step at a time
+    for t in range(max_target_length):
+        decoder_output, decoder_hidden, decoder_attn = decoder(
+            decoder_input, decoder_hidden, encoder_outputs
+        )
+
+        all_decoder_outputs[t] = decoder_output
+        decoder_input = target_batches[t]  # Next input is current target
+
+    # Loss calculation and backpropagation
+    loss = masked_cross_entropy(
+        all_decoder_outputs.transpose(0, 1).contiguous(),  # -> batch x seq
+        target_batches.transpose(0, 1).contiguous(),  # -> batch x seq
+        target_lengths
+    )
+    loss.backward()
+
+    # Clip gradient norms
+    ec = torch.nn.utils.clip_grad_norm(encoder.parameters(), clip)
+    dc = torch.nn.utils.clip_grad_norm(decoder.parameters(), clip)
+
+    # Update parameters with optimizers
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.data[0], ec, dc
+
+
+def as_minutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def time_since(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
+
+
+def evaluate(input_seq, max_length=MAX_LENGTH):
+    input_lengths = [len(input_seq)]
+    input_seqs = [indexes_from_sentence(input_lang, input_seq)]
+    input_batches = Variable(torch.LongTensor(input_seqs), volatile=True).transpose(0, 1)
+
+    if USE_CUDA:
+        input_batches = input_batches.cuda()
+
+    # Set to not-training mode to disable dropout
+    encoder.train(False)
+    decoder.train(False)
+
+    # Run through encoder
+    encoder_outputs, encoder_hidden = encoder(input_batches, input_lengths, None)
+
+    # Create starting vectors for decoder
+    decoder_input = Variable(torch.LongTensor([SOS_token]), volatile=True)  # SOS
+    decoder_hidden = encoder_hidden[:decoder.n_layers]  # Use last (forward) hidden state from encoder
+
+    if USE_CUDA:
+        decoder_input = decoder_input.cuda()
+
+    # Store output words and attention states
+    decoded_words = []
+    decoder_attentions = torch.zeros(max_length + 1, max_length + 1)
+
+    # Run through decoder
+    for di in range(max_length):
+        decoder_output, decoder_hidden, decoder_attention = decoder(
+            decoder_input, decoder_hidden, encoder_outputs
+        )
+        decoder_attentions[di, :decoder_attention.size(2)] += decoder_attention.squeeze(0).squeeze(0).cpu().data
+
+        # Choose top word from output
+        topv, topi = decoder_output.data.topk(1)
+        ni = topi[0][0]
+        if ni == EOS_token:
+            decoded_words.append('<EOS>')
+            break
+        else:
+            decoded_words.append(output_lang.index2word[ni])
+
+        # Next input is chosen word
+        decoder_input = Variable(torch.LongTensor([ni]))
+        if USE_CUDA: decoder_input = decoder_input.cuda()
+
+    # Set back to training mode
+    encoder.train(True)
+    decoder.train(True)
+
+    return decoded_words, decoder_attentions[:di + 1, :len(encoder_outputs)]
+
+
+def evaluate_randomly():
+    [input_sentence, target_sentence] = random.choice(pairs)
+    evaluate_and_show_attention(input_sentence, target_sentence)
+
+
+def show_plot_visdom():
+    buf = io.BytesIO()
+    plt.savefig(buf)
+    buf.seek(0)
+    attn_win = 'attention (%s)' % 'bikash'
+    vis.image(torchvision.transforms.ToTensor()(Image.open(buf)), win=attn_win, opts={'title': attn_win})
+
+
+def show_attention(input_sentence, output_words, attentions):
+    # Set up figure with colorbar
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(attentions.numpy(), cmap='bone')
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + input_sentence.split(' ') + ['<EOS>'], rotation=90)
+    ax.set_yticklabels([''] + output_words)
+
+    # Show label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    show_plot_visdom()
+    plt.show()
+    plt.close()
+
+
+def evaluate_and_show_attention(input_sentence, target_sentence=None):
+    output_words, attentions = evaluate(input_sentence)
+    output_sentence = ' '.join(output_words)
+    print('>', input_sentence)
+    if target_sentence is not None:
+        print('=', target_sentence)
+    print('<', output_sentence)
+
+    show_attention(input_sentence, output_words, attentions)
+
+    # # Show input, target, output text in visdom
+    # win = 'evaluted (%s)' % hostname
+    # text = '<p>&gt; %s</p><p>= %s</p><p>&lt; %s</p>' % (input_sentence, target_sentence, output_sentence)
+    # vis.text(text, win=win, opts={'title': win})
+
+
 if __name__ == '__main__':
     USE_CUDA = torch.cuda.is_available()
     input_lang, output_lang, pairs = prepare_data('E:\\Datasets\\NMT\\fra.txt', 'eng', 'fra', True)
@@ -214,29 +451,111 @@ if __name__ == '__main__':
     print("Trimmed from %d pairs to %d, %.4f of total" % (len(pairs), len(keep_pairs), len(keep_pairs) / len(pairs)))
     pairs = keep_pairs
 
-    # Test the model
-    print("\n---------------------")
-    print("Testing the model....")
-    small_batch_size = 3
-    input_batches, input_lengths, target_batches, target_lengths = random_batch(small_batch_size)
+    # test the model
+    test_model()
 
-    print('input_batches', input_batches.size())  # (max_len x batch_size)
-    print('target_batches', target_batches.size())  # (max_len x batch_size)
+    # Configure models
+    attn_model = 'dot'
+    hidden_size = 500
+    n_layers = 2
+    dropout = 0.1
+    batch_size = 100
+    batch_size = 50
 
-    small_hidden_size = 8
-    small_n_layers = 2
+    # Configure training/optimization
+    clip = 50.0
+    teacher_forcing_ratio = 0.5
+    learning_rate = 0.0001
+    decoder_learning_ratio = 5.0
+    n_epochs = 50000
+    epoch = 0
+    plot_every = 20
+    print_every = 100
+    evaluate_every = 1000
 
-    encoder_test = EncoderRNN(input_lang.n_words, small_hidden_size, small_n_layers)
-    decoder_test = LuongAttnDecoderRNN('general', small_hidden_size, output_lang.n_words, small_n_layers)
+    # Initialize models
+    encoder = EncoderRNN(input_lang.n_words, hidden_size, n_layers, dropout=dropout)
+    decoder = LuongAttnDecoderRNN(attn_model, hidden_size, output_lang.n_words, n_layers, dropout=dropout)
 
+    # Initialize optimizers and criterion
+    encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate * decoder_learning_ratio)
+    criterion = nn.CrossEntropyLoss()
+
+    # Move models to GPU
     if USE_CUDA:
-        encoder_test.cuda()
-        decoder_test.cuda()
+        encoder.cuda()
+        decoder.cuda()
 
-    encoder_outputs, encoder_hidden = encoder_test(input_batches, input_lengths, None)
+    import sconce
 
-    print('encoder_outputs', encoder_outputs.size())  # max_len x batch_size x hidden_size
-    print('encoder_hidden', encoder_hidden.size())  # n_layers * 2 x batch_size x hidden_size
+    job = sconce.Job('seq2seq-translate', {
+        'attn_model': attn_model,
+        'n_layers': n_layers,
+        'dropout': dropout,
+        'hidden_size': hidden_size,
+        'learning_rate': learning_rate,
+        'clip': clip,
+        'teacher_forcing_ratio': teacher_forcing_ratio,
+        'decoder_learning_ratio': decoder_learning_ratio,
+    })
+    job.plot_every = plot_every
+    job.log_every = print_every
 
+    # Keep track of time elapsed and running averages
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
 
+    # Begin!
+    ecs = []
+    dcs = []
+    eca = 0
+    dca = 0
 
+    while epoch < n_epochs:
+        epoch += 1
+
+        # Get training data for this cycle
+        input_batches, input_lengths, target_batches, target_lengths = random_batch(batch_size)
+
+        # Run the train function
+        loss, ec, dc = train(
+            input_batches, input_lengths, target_batches, target_lengths,
+            encoder, decoder,
+            encoder_optimizer, decoder_optimizer, criterion
+        )
+
+        # Keep track of loss
+        print_loss_total += loss
+        plot_loss_total += loss
+        eca += ec
+        dca += dc
+
+        job.record(epoch, loss)
+
+        if epoch % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print_summary = '%s (%d %d%%) %.4f' % (
+                time_since(start, epoch / n_epochs), epoch, epoch / n_epochs * 100, print_loss_avg)
+            print(print_summary)
+
+        if epoch % evaluate_every == 0:
+            evaluate_randomly()
+
+        if epoch % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+
+            # TODO: Running average helper
+            ecs.append(eca / plot_every)
+            dcs.append(dca / plot_every)
+            ecs_win = 'encoder grad (%s)' % hostname
+            dcs_win = 'decoder grad (%s)' % hostname
+            # vis.line(np.array(ecs), win=ecs_win, opts={'title': ecs_win})
+            # vis.line(np.array(dcs), win=dcs_win, opts={'title': dcs_win})
+            eca = 0
+            dca = 0
